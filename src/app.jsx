06b391b332c1cc40e0b36627c43a858060e98426
Khai Hirschi <orangekocelot@gmail.com>
2023-04-08 00:08:37 -0600

    fix(author)

--- src/app.jsx
+++ src/app.jsx
@@ -90,7 +90,7 @@ function App() {
 
       <footer className='bg-dark text-dark text-muted'>
         <div className='container-fluid'>
-          <span className='text-reset'>Author Name(s)</span>
+          <span className='text-reset'>Khai Hirschi</span>
           <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
             Source
           </a>
